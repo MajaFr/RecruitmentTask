62deaeada91419f67d70e0bc184f828c7a4eb10b
Maja <maja.frak@payair.com>
2025-05-22 14:32:46 +0200

    Initial Commit

---
+++ RecruitmentTask/RecruitmentTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecruitmentTaskApp.swift
+//  RecruitmentTask
+//
+//  Created by Maja Frąk on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecruitmentTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
